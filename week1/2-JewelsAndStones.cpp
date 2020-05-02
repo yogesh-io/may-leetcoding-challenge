e2a392d5a3111e1876258e9805302cfaf8fd2ec1
Yogesh Yadav <2803yogesh.yadav@gmail.com>
2020-05-03 01:38:41 +0530

    update challenge qna

--- week1/2-JewelsAndStones.cpp
+++ week1/2-JewelsAndStones.cpp
@@ -27,3 +27,20 @@ public:
         
     }
 };
+"""Alternatively"""
+
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) {
+        unordered_set<char>set;
+        int count=0;
+        for(int i=0;i<J.length();i++)
+            set.insert(J[i]);
+        for(int i=0;i<S.length();i++)
+        {
+            if(find(set.begin(),set.end(),S[i])!=set.end())
+                count++;
+        }
+        return count;
+    }
+};
