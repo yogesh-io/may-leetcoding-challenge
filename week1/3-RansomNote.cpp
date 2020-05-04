7c01d777399047db0c21dbd08b1ee06f72c0f8b4
Yogesh Yadav <2803yogesh.yadav@gmail.com>
2020-05-04 11:52:33 +0530

    adding day 3 code

---
+++ week1/3-RansomNote.cpp
@@ -0,0 +1,44 @@
+'''
+Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+
+Each letter in the magazine string can only be used once in your ransom note.
+
+Note:
+You may assume that both strings contain only lowercase letters.
+
+canConstruct("a", "b") -> false
+canConstruct("aa", "ab") -> false
+canConstruct("aa", "aab") -> true
+
+'''
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        if(ransomNote.length()>magazine.length())
+            return false;
+        else
+        {        
+            int count=0;
+            for(char a: ransomNote)
+            {   for(int i=0;i<magazine.length();i++)
+                {
+                    if(a==magazine[i])
+                    {   count++;
+                        magazine.erase(magazine.begin() + i);
+                        break;
+                    }
+                }        
+            
+            }
+        
+            if(ransomNote.length()==count)
+              return true;
+            else 
+              return false;
+            
+         }             
+        }
+};
+
+
